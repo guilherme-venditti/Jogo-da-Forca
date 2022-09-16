f134b370d976216620c39832a936a6a622916efb
Guilherme Venditti <112132085+guilherme-venditti@users.noreply.github.com>
2022-09-15 21:13:00 -0300

    Função de desenhar letras erradas criada

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Seletores
 //let palavras = ["ALURA", "ORACLE", "FORCA", "JAVASCRIPT", "LOGICA"]
-let palavras = ["ABAB"]
+let palavras = ["JAVASCRIPT"]
 let tabuleiro = document.getElementById('forca').getContext('2d')
 let palavraSecreta = "";
 
@@ -10,25 +10,37 @@ function sorteiaPalavra() {
     console.log(palavra)
 }
 
-function verificaLetra() {
+function jogar() {
+    let erros = 0;
     document.addEventListener('keypress', (event) => {
-        let chave = event.key.toUpperCase()
-
+    let chave = event.key.toUpperCase()
+    
         if (chave.length == 1 && chave.match(/[a-z]/i)) {
-            const indexes = []
-            
-           if (palavraSecreta.includes(chave)) {
-            for (let i = 0; i < palavraSecreta.length; i++) {
-                if (palavraSecreta[i] === chave) {
-                indexes.push(i) }} //Irá enviar para a const indexes a posição das letras repetidas, caso haja.
+                const indexes = []
+                let letrasErradas = []
+                
+            //Irá enviar para a const indexes a posição das letras repetidas, caso haja.
+            if (palavraSecreta.includes(chave)) {
+                for (let i = 0; i < palavraSecreta.length; i++) {
+                    if (palavraSecreta[i] === chave) {
+                    indexes.push(i) }} 
 
-                for (let p = 0; p < indexes.length; p++){
-                    desenhaLetra(chave, indexes[p]) //Desenha as letras usando as posições presentes no index.
-                }
+                    //Desenha as letras usando as posições presentes no index.
+                    for (let p = 0; p < indexes.length; p++){
+                        desenhaLetra(chave, indexes[p])
+                }   
+            } else {
+                erros++
+                letrasErradas.push(chave)
+                if (letrasErradas.indexOf(chave) != -1) {
+                desenhaLetraErrada(chave, erros)
+               }
+               console.log(erros)
             }
         }
     }
-    )}
+)}
+
 
 function desenhaLetra(letra, pos) {
     let largura = 600/palavraSecreta.length
@@ -37,11 +49,19 @@ function desenhaLetra(letra, pos) {
     tabuleiro.fillText(letra, 500+(largura*pos), 600)
 }
 
+function desenhaLetraErrada(letra, tentativa) {
+    let largura = 600/palavraSecreta.length
+    tabuleiro.fillStyle = 'red'
+    tabuleiro.font = '60px san-serif';
+    tabuleiro.fillText(letra, 500+(largura*tentativa), 710)
+
+}
+
 function iniciarJogo() {
     document.getElementById('desaparece').style.display = 'none'
     document.getElementById('forca').style.display = 'block'
     sorteiaPalavra()
     desenhaCanvas()
     desenhaLinhas()
-    verificaLetra()
+    jogar()
 }
